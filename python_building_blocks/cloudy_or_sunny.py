f53140f7792021cf315a07961aba3789495714eb
ZeNGrooT147 <suhasgosal147@gmail.com>
2024-12-10 16:50:16 +0530

    New edit

--- python_building_blocks/cloudy_or_sunny.py
+++ python_building_blocks/cloudy_or_sunny.py
@@ -1,25 +1,26 @@
 """ 8. Check if it is cloudy or sunny. If sunny and hot, then go out with jacket. If only sunny the go out as you wish.
 If it is cloudy, then check if it is raining. IF it is only cloudy then go out as you wish. IF rainy go out with umbrella."""
 
-cloudy or sunny
-
-if sunny and hot ==then go out with jacket
-if only sunny == then go out as you wish
-if it it is cloudy == then check if it is raining =if rainy go out with umbrella
-
 print("Whether the atmosphere is cloudy or sunny(enter cloudy or sunny)")
 
-str1=input(())
+str1=input()
 
 if (str1=='cloudy'):
     print("Whether It is Raining:(yes or no)")
-    str2=input(())
+    str2=input()
     if str2=='yes':
         print('Go out with umbrella')
     else:
         print('Go out as you Wish ')
 elif (str1=='sunny'):
-    print('Whether it is Sunny and hot : (yes or no)')
-    if str3=='yes':
+    print('Whether it is Sunny and Hot or just Sunny (enter both or sunny)')
+    str3=input()
+    if str3 =='both':
+        print('Go out with jacket')
+    elif str3 == 'sunny' :
         print(' Go out as you wish ')
+    else :
+            print('Wrong input')
+else:
+    print('Wrong input')
     
